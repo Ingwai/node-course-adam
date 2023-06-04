8eeb3d1bce35dbe5edd80636a24e1a279c124955
Ingwai <ingwai@wp.pl>
2023-06-04 15:17:36 +0200

    added pagination, filter, sorting, searching

--- app/controllers/company-controller.js
+++ app/controllers/company-controller.js
@@ -2,10 +2,43 @@ const Company = require('../db/models/company');
 
 class CompanyController {
 	async showCompanies(req, res) {
-		const companies = await Company.find({});
-		console.log(companies);
+		const { q, sort, countmin, countmax } = req.query;
+		const page = req.query.page || 1;
+		const perPage = 2;
+		const where = {};
+
+		// search
+		if (q) where.name = { $regex: q, $options: 'i' };
+
+		// filter
+		if (countmin || countmax) {
+			where.employeesCount = {};
+			if (countmin) where.employeesCount.$gte = countmin;
+			if (countmax) where.employeesCount.$lte = countmax;
+		}
+
+		let query = Company.find(where);
+
+		// pagination
+		query = query.skip((page - 1) * perPage);
+		query = query.limit(perPage); // ile wyników na stronę ustawiam limit
+
+		// sort
+		// opcje wyrażeń regularnych ma mongoDB
+		if (sort) {
+			const s = sort.split('|');
+			query = query.sort({ [s[0]]: s[1] }); //jest w MongoDB funkcja sort
+		}
+		// egzekwowanie (exec)
+		const companies = await query.exec();
+		const resultsCount = await Company.find(where).count();
+		const pagesCount = Math.ceil(resultsCount / perPage);
+		// exec() że uruchamiam wszystkie moje parametry
 		res.render('pages/companies/companies', {
 			companies,
+			page,
+			pagesCount,
+			resultsCount,
 		});
 	}
 
--- app/middleware/view-variables.js
+++ app/middleware/view-variables.js
@@ -3,5 +3,6 @@ module.exports = (req, res, next) => {
 	res.locals.url = req.url;
 	res.locals.errors = null;
 	res.locals.form = {};
+	res.locals.query = req.query;
 	next();
 };
