93d7229743146e270f5fcbdaf7e00262f4ec7bc2
Ingwai <ingwai@wp.pl>
2023-06-09 10:36:26 +0200

    added to api auth and login

--- app/middleware/is-auth-api-middleware.js
+++ app/middleware/is-auth-api-middleware.js
@@ -1,6 +1,20 @@
-module.exports = (req, res, next) => {
+const User = require('../db/models/user');
+
+module.exports = async (req, res, next) => {
 	if (true) {
-		res.status(403).json({ message: 'Nie masz dostępu' });
+		console.log(req.headers);
+		const token = req.headers.authorization?.split('')[1];
+		if (!token) {
+			res.status(403).json({ message: 'Nie masz dostępu' });
+		}
+
+		const user = await User.findOne({ apitoken: token });
+
+		if (!user) {
+			res.status(403).json({ message: 'Nie masz dostępu' });
+		}
+
+		req.user = user;
 	}
 	next();
 };
